e08b0edeb7f51a5b6a244f2a7173e86d4e1a2b4b
RamonBecker <ramonbecker68@gmail.com>
2021-06-08 18:34:25 -0300

    Criando objeto DTO para Categoria

--- gestao-vendas/src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
+++ gestao-vendas/src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
@@ -2,6 +2,7 @@ package com.gvendas.gestaovendas.controlador;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -18,12 +19,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gvendas.gestaovendas.dto.CategoriaResponseDTO;
 import com.gvendas.gestaovendas.entidades.Categoria;
 import com.gvendas.gestaovendas.servico.CategoriaServico;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-
 
 @Api(tags = "Categoria")
 @RestController
@@ -35,23 +36,29 @@ public class CategoriaControlador {
 
 	@ApiOperation(value = "Listar todas as categorias", nickname = "listarTodas")
 	@GetMapping
-	public List<Categoria> listarTodas() {
-		return categoriaServico.listarTodas();
+	public List<CategoriaResponseDTO> listarTodas() {
+		// 1 - Retornando as categorias do banco de dados
+		// 2 - Chamando o metodo estatico da CategoriaResponseDTO
+		// 3- E percorrendo a lista do banco e transformando cada objeto de Categoria
+		// para CategoriaResponseDTO
+		return categoriaServico.listarTodas().stream()
+				.map(categoria -> CategoriaResponseDTO.conversaoParaCategoriaDTO(categoria))
+				.collect(Collectors.toList());
 	}
 
-	@ApiOperation(value = "Listar por código", nickname = "buscarPorId" )
+	@ApiOperation(value = "Listar por código", nickname = "buscarPorId")
 	@GetMapping("/{codigo}")
-	public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable Long codigo) {
+	public ResponseEntity<CategoriaResponseDTO> buscarPorId(@PathVariable Long codigo) {
 		Optional<Categoria> categoria = categoriaServico.buscarPorCodigo(codigo);
 
 		// Verificando se a categoria que será buscado existe no banco de dados
 		if (categoria.isPresent()) {
-			return ResponseEntity.ok(categoria);
+			return ResponseEntity.ok(CategoriaResponseDTO.conversaoParaCategoriaDTO(categoria.get()));
 		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
+
 	@ApiOperation(value = "Salvar", nickname = "salvarCategoria")
 
 	@PostMapping
@@ -60,18 +67,18 @@ public class CategoriaControlador {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
-	
+
 	@ApiOperation(value = "AtualizarCategoria", nickname = "AtualizarCategoria")
 	@PutMapping("/{codigo}")
-	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo,@Valid @RequestBody Categoria categoria) {
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @Valid @RequestBody Categoria categoria) {
 		return ResponseEntity.ok(categoriaServico.atualizar(codigo, categoria));
 	}
-	
+
 	@ApiOperation(value = "DeletarCategoria", nickname = "DeletarCategoria")
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void deletar(@PathVariable Long codigo) {
 		categoriaServico.deletar(codigo);
 	}
-	
+
 }
---
+++ gestao-vendas/src/main/java/com/gvendas/gestaovendas/dto/CategoriaResponseDTO.java
@@ -0,0 +1,42 @@
+package com.gvendas.gestaovendas.dto;
+
+import com.gvendas.gestaovendas.entidades.Categoria;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("CategoriaRetornoDTO")
+public class CategoriaResponseDTO {
+
+	public CategoriaResponseDTO(Long codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public static CategoriaResponseDTO conversaoParaCategoriaDTO(Categoria categoria) {
+		return new CategoriaResponseDTO(categoria.getCodigo(), categoria.getNome());
+	}
+
+	@ApiModelProperty(value = "Código")
+	private Long codigo;
+
+	@ApiModelProperty(value = "Nome")
+	private String nome;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
