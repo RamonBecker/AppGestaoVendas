4c69e2010fefce286bb0585d78f5ea415c668a6d
RamonBecker <ramonbecker68@gmail.com>
2021-06-12 15:43:10 -0300

    Criando entidade cliente e mapeando com JPA

---
+++ gestao-vendas/src/main/java/com/gvendas/gestaovendas/entidades/Cliente.java
@@ -0,0 +1,96 @@
+package com.gvendas.gestaovendas.entidades;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cliente")
+public class Cliente {
+	@Id
+	// Auto incremento
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigo")
+	private Long codigo;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "telefone")
+	private String telefone;
+
+	@Column(name = "ativo")
+	private boolean ativo;
+
+	// Injetando a classe endereco na tabela do cliente
+	@Embedded
+	private Endereco endereco;
+
+	public Cliente() {
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ativo, codigo, endereco, nome, telefone);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Cliente)) {
+			return false;
+		}
+		Cliente other = (Cliente) obj;
+		return ativo == other.ativo && Objects.equals(codigo, other.codigo) && Objects.equals(endereco, other.endereco)
+				&& Objects.equals(nome, other.nome) && Objects.equals(telefone, other.telefone);
+	}
+
+}
